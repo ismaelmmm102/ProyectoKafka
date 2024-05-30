257f2abbd7bb71c91272928e21d294759d086262
ismaelmmm102 <149151142+ismaelmmm102@users.noreply.github.com>
2024-05-30 08:49:22 +0200

    Add files via upload

---
+++ Consumer/src/main/java/com/microservicios_kafka/api_rest_kafka/ApiRestKafkaApplication.java
@@ -0,0 +1,11 @@
+package com.microservicios_kafka.api_rest_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestKafkaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestKafkaApplication.class, args);
+	}	
+}
---
+++ Consumer/src/test/java/com/microservicios_kafka/api_rest_kafka/ApiRestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios_kafka.api_rest_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

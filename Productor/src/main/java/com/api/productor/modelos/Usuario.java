7e0cbd82014c4ba44458cd47af478c796b40cac6
ismaelmmm102 <149151142+ismaelmmm102@users.noreply.github.com>
2024-06-05 13:12:56 +0200

    Add files via upload

---
+++ Productor/src/main/java/com/api/productor/modelos/Usuario.java
@@ -0,0 +1,35 @@
+package com.api.productor.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String edad;
+    
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id=id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getEdad() {
+		return edad;
+	}
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+    
+}

107d1f0a3efc270d813035b06cd99a7b3beb7e0f
ismaelmmm102 <149151142+ismaelmmm102@users.noreply.github.com>
2024-06-05 13:47:53 +0200

    Delete Consumidor directory

--- Consumidor/src/main/java/com/api/consumidor/repositorios/UsuarioRepo.java
+++
@@ -1,8 +0,0 @@
-package com.api.consumidor.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.consumidor.modelos.Usuario;
-
-public interface UsuarioRepo extends JpaRepository<Usuario, String> {
-}
